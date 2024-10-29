4a7cb9422f3f0e5293a79a6a21a899781eb9c4f8
IgSka <gaydukovgo@gmail.com>
2024-10-29 21:37:44 +0300

    Добавлена функция загрузки электронных адресов и пароля из файла save.txt

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def save():
         file.write(password_entry.get() +"\n")
 
 
+def load():
+    try:
+        with open("save.txt", "r") as file:
+            info = file.readlines()
+            sender_email_entry.insert(0, info[0])
+            recipient_email_entry.insert(0,info[1])
+            password_entry.insert(0, info[2])
+    except FileNotFoundError:
+        pass
+
+
 def send_email():
     save()
     sender_email = sender_email_entry.get()
@@ -71,6 +82,9 @@ body_text.grid(row=4, column=1, sticky=W)
 Button(text="Отправить письмо", command = send_email).grid(row=5, column =1, sticky=W)
 result_label = Label(text=" ")
 result_label.grid(row=6, column =1, sticky=W)
+
+
+load()
 
 window.mainloop()
 
